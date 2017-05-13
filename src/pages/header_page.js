57733379287d07ebaeaf1e6098412195437f20bc
Daniel Cawen <daniel@oktana.io>
2017-05-13 16:49:51 -0300

    Added waitForExists and waitForVisible. Added callback (done();).

--- src/pages/header_page.js
+++ src/pages/header_page.js
@@ -7,13 +7,17 @@ module.exports = {
       userProfileImage: '.oneUserProfileCardTrigger .tooltipTrigger .profileTrigger'
   	},
 
-    getUsername: function(){
+    getUsername: function(done){
     	var hp = this.headerPage;
-    	
-    	browser.waitForExist(hp.userProfileImage,20000);
+
+    	browser.waitForExist(hp.userProfileImage,30000);
     	browser.click(hp.userProfileImage);
-    	browser.waitForExist(hp.usernameLabel,20000)
+
+    	browser.waitForExist(hp.usernameLabel,30000);
+    	browser.waitForVisible(hp.usernameLabel,30000);
+    	
     	return browser.getText(hp.usernameLabel);
+    	done();
     }
     
 };
